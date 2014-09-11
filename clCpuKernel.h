38e3a4b8a65fa6e4fb4f80933fcac821a4aee810
Andrey Balabokhin <abalabokhin@gmail.com>
2014-09-11 10:53:24 -0400

    cleaned code

--- clCpuKernel.h
+++ clCpuKernel.h
@@ -16,14 +16,18 @@ public:
 
     template<typename... Values> void RunKernel(KernelRunningSettings const & runningSettings, Values... values) {
         std::function<void(KernelRunningSettings)> newFn = AddArgs(std::function<Fn>(function), values...);
-        /*if (runningSettings.getGlobalId(2) != 0) {
+        if (runningSettings.getGlobalId(2) != 0) {
             for (size_t i1 = 0; i1 < runningSettings.getGlobalId(0); ++i1) {
                 for (size_t i2 = 0; i2 < runningSettings.getGlobalId(1); ++i2) {
                     for (size_t i3 = 0; i3 < runningSettings.getGlobalId(2); ++i3) {
                         newFn(KernelRunningSettings(
                             {i1, i2, i3},
                             {i1 / runningSettings.getLocalId(0), i2 / runningSettings.getLocalId(1), i3 / runningSettings.getLocalId(2)}));
-                        //function(values..., CpuRange({i1, i2, i3}));
+//                        function(
+//                            KernelRunningSettings(
+//                                {i1, i2, i3},
+//                                {i1 / runningSettings.getLocalId(0), i2 / runningSettings.getLocalId(1), i3 / runningSettings.getLocalId(2)}),
+//                            values...);
                     }
                 }
             }
@@ -35,7 +39,11 @@ public:
                     newFn(KernelRunningSettings(
                         {i1, i2},
                         {i1 / runningSettings.getLocalId(0), i2 / runningSettings.getLocalId(1)}));
-                    //function(values..., CpuRange({i1, i2, 0}));
+//                    function(
+//                        KernelRunningSettings(
+//                            {i1, i2},
+//                            {i1 / runningSettings.getLocalId(0), i2 / runningSettings.getLocalId(1)}),
+//                        values...);
                 }
             }
             return;
@@ -45,8 +53,12 @@ public:
             newFn(KernelRunningSettings(
                 {i1},
                 {i1 / runningSettings.getLocalId(0)}));
-            //function(values..., CpuRange({i1, 0, 0}));
-        }*/
+//            function(
+//                KernelRunningSettings(
+//                    {i1},
+//                    {i1 / runningSettings.getLocalId(0)}),
+//                values...);
+        }
     }
 
 private:
@@ -84,11 +96,6 @@ private:
     std::function<void(KernelRunningSettings, FAT2, FAT3, FAT4, FAT5)> SetArg(std::function<void(KernelRunningSettings, FAT1, FAT2, FAT3, FAT4, FAT5)> fn, ArgsT value) {
         return std::bind(fn, std::placeholders::_1, value, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5);
     }
-
-    /*template <typename ArgsT, typename FuncArgsT, typename... Tail>
-    std::function<void(Tail... tail, KernelRunningSettings)> SetArg(std::function<void(FuncArgsT, Tail... tail, KernelRunningSettings)> fn, ArgsT value) {
-        return std::bind(fn, value, std::placeholders::_1);
-    }*/
 private:
     Fn * function;
 };
