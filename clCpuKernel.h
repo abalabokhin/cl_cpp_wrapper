157faf8153a06c8f27d4c13374a6c86503ae711d
Andrey Balabokhin <abalabokhin@gmail.com>
2014-09-11 09:59:46 -0400

    improved templates

--- clCpuKernel.h
+++ clCpuKernel.h
@@ -15,9 +15,8 @@ public:
     {}
 
     template<typename... Values> void RunKernel(KernelRunningSettings const & runningSettings, Values... values) {
-        std::function<void(KernelRunningSettings)> newFn = AddArgs(std::function<Fn>(function), values...);
-        //auto globalRange = runningSettings.get_global_id();
-        if (runningSettings.getGlobalId(2) != 0) {
+        //std::function<void(KernelRunningSettings)> newFn = AddArgs(std::function<Fn>(function), values...);
+        /*if (runningSettings.getGlobalId(2) != 0) {
             for (size_t i1 = 0; i1 < runningSettings.getGlobalId(0); ++i1) {
                 for (size_t i2 = 0; i2 < runningSettings.getGlobalId(1); ++i2) {
                     for (size_t i3 = 0; i3 < runningSettings.getGlobalId(2); ++i3) {
@@ -47,20 +46,23 @@ public:
                 {i1},
                 {i1 / runningSettings.getLocalId(0)}));
             //function(values..., CpuRange({i1, 0, 0}));
-        }
+        }*/
     }
 
 private:
-    std::function<void(KernelRunningSettings)> AddArgs(std::function<void(KernelRunningSettings)> fn) {
+    std::function<void(KernelRunningSettings)>
+    AddArgs(std::function<void(KernelRunningSettings)> fn) {
         return fn;
     }
 
-    template<typename T, typename... Tail> std::function<void(Tail... tail, KernelRunningSettings)> AddArgs(std::function<void(T, Tail... tail, KernelRunningSettings)> fn, T head, Tail... tail) {
+    template<typename ArgsT, typename FuncArgsT, typename... ArgsTail, typename... FuncArgsTail>
+    std::function<void(FuncArgsTail... tail, KernelRunningSettings)> AddArgs(std::function<void(FuncArgsT, FuncArgsTail... tail, KernelRunningSettings)> fn, ArgsT head, ArgsTail... tail) {
         return AddArgs(SetArg(fn, head), tail...);
     }
 
-    template <typename T, typename... Tail> std::function<void(Tail... tail, KernelRunningSettings)> SetArg(std::function<void(T, Tail... tail, KernelRunningSettings)> fn, T value) {
-        return std::bind(fn, std::placeholders::_1, value);
+    template <typename ArgsT, typename FuncArgsT, typename... Tail>
+    std::function<void(Tail... tail, KernelRunningSettings)> SetArg(std::function<void(FuncArgsT, Tail... tail, KernelRunningSettings)> fn, ArgsT value) {
+        return std::bind(fn, value, std::placeholders::_1);
     }
 private:
     Fn * function;
